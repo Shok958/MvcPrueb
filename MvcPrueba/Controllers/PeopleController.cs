acf436e376fafd8e9d0463e55a846eca660dbb04
Antonio <antonio958@hotmail.com>
2017-11-29 16:35:42 +0100

    Añadidas Validaciones

--- MvcPrueba/Controllers/PeopleController.cs
+++ MvcPrueba/Controllers/PeopleController.cs
@@ -10,124 +10,107 @@ using MvcPrueba.Models;
 
 namespace MvcPrueba.Controllers
 {
-    public class MoviesController : Controller
+    public class PeopleController : Controller
     {
-        private MovieDBContext db = new MovieDBContext();
+        private PersonDBContext db = new PersonDBContext();
 
-        // GET: Movies
-        public ActionResult Index(string movieGenre, string searchString)
+        // GET: People
+        public ActionResult Index()
         {
-            var GenreLst = new List<string>();
-            var GenreQry = from d in db.Movies
-                           orderby d.Genre
-                           select d.Genre;
-            GenreLst.AddRange(GenreQry.Distinct());
-            ViewBag.movieGenre = new SelectList(GenreLst);
-
-            var movies = from m in db.Movies
-                         select m;
-            if (!String.IsNullOrEmpty(searchString))
-            {
-                movies = movies.Where(s => s.Title.Contains(searchString));
-            }
-            if (!String.IsNullOrEmpty(movieGenre))
-            {
-                movies = movies.Where(s => s.Genre == movieGenre);
-            }
-            return View(movies);
+            return View(db.Persons.ToList());
         }
 
-        // GET: Movies/Details/5
+        // GET: People/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Movie movie = db.Movies.Find(id);
-            if (movie == null)
+            Person person = db.Persons.Find(id);
+            if (person == null)
             {
                 return HttpNotFound();
             }
-            return View(movie);
+            return View(person);
         }
 
-        // GET: Movies/Create
+        // GET: People/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: Movies/Create
+        // POST: People/Create
         // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
         // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,Title,ReleaseDate,Genre,Rating,Price")] Movie movie)
+        public ActionResult Create([Bind(Include = "ID,Name,SubName,Email,ReleaseDate")] Person person)
         {
             if (ModelState.IsValid)
             {
-                db.Movies.Add(movie);
+                db.Persons.Add(person);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
-            return View(movie);
+            return View(person);
         }
 
-        // GET: Movies/Edit/5
+        // GET: People/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Movie movie = db.Movies.Find(id);
-            if (movie == null)
+            Person person = db.Persons.Find(id);
+            if (person == null)
             {
                 return HttpNotFound();
             }
-            return View(movie);
+            return View(person);
         }
 
-        // POST: Movies/Edit/5
+        // POST: People/Edit/5
         // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
         // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,Title,ReleaseDate,Genre,Rating,Price")] Movie movie)
+        public ActionResult Edit([Bind(Include = "ID,Name,SubName,Email,ReleaseDate")] Person person)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(movie).State = EntityState.Modified;
+                db.Entry(person).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            return View(movie);
+            return View(person);
         }
 
-        // GET: Movies/Delete/5
+        // GET: People/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Movie movie = db.Movies.Find(id);
-            if (movie == null)
+            Person person = db.Persons.Find(id);
+            if (person == null)
             {
                 return HttpNotFound();
             }
-            return View(movie);
+            return View(person);
         }
 
-        // POST: Movies/Delete/5
+        // POST: People/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            Movie movie = db.Movies.Find(id);
-            db.Movies.Remove(movie);
+            Person person = db.Persons.Find(id);
+            db.Persons.Remove(person);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
---
+++ MvcPrueba/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcPrueba.Models
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string SubName { get; set; }
+        public string Email { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+
+    public class PersonDBContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+    }
+}
