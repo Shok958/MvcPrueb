81aa00075260864484863cbf14a189aba0e1955a
Antonio <antonio958@hotmail.com>
2017-11-29 11:32:37 +0100

    Primera Prueba MVC

---
+++ MvcPrueba/Controllers/HwController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcPrueba.Controllers
+{
+    public class HwController : Controller
+    {
+        // GET: Hw
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewBag.Message = "Hello" + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+        }
+    }
+}
